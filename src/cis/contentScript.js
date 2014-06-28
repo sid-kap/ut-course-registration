943a99a2d29e10233c866b315be0c945b968a8de
Sidharth Kapur <sidharthkapur1@gmail.com>
2014-06-28 11:26:36 -0500

    Added bar graphs to CIS.

---
+++ src/cis/contentScript.js
@@ -0,0 +1,97 @@
+
+var $ = jQuery;
+
+$(function() {
+	$('tbody').each(function() {
+		var rows,
+		    tds,
+		    $td, 
+		    widths,
+		    width,
+		    labels = [],
+		    floatWidth,
+		    displayFloatWidth,
+		    cssWidth,
+		    text,
+		    $tdChart,
+		    i,
+		    j,
+		    cssIndex;
+
+		rows = $(this).children();
+		for (i = 0; i < rows.length; i++) {
+			tds = $(rows[i]).children();
+			widths = [];
+			$tdChart = $(tds[1]);
+
+			for (j = 1; j < 6; j++) {
+				$td = $(tds[j]);
+
+				if (i === 0) {
+					labels.push($td.text());
+				} else {
+					widths.push($td.text());
+				}
+
+				if (j === 1) {
+					$td.text('');
+				} else {
+					$td.remove();
+				}
+			}
+
+			$tdChart.css('width', '20%');
+
+			if (i != 0) {
+				for (var j in widths) {
+
+					// To display the correct background color of the bar
+					// we must add one of these css classes: .chart-1, .chart-2, .chart-3, .chart-4 , .chart-5 
+					cssIndex = parseInt(j)+1;
+					width = widths[j];
+
+					floatWidth = parseFloatFromPercentage(width);
+
+					displayFloatWidth = 0.95 * floatWidth;
+
+					// Shitty algorithm to make sure the table cell doesn't overflow
+					// into two rows
+					// if (displayFloatWidth > 30)
+					// 	displayFloatWidth -= 2.5;
+
+					cssWidth = displayFloatWidth.toFixed(2) + '%';
+
+					text = '&nbsp';
+
+					// Only display the number in the cell if it's big enough for 
+					// the text to fit
+					// (Arbitrarily chose 10% because it looks good in my browser)
+					if (floatWidth > 10 /* percent */) {
+						text = floatWidth.toFixed(0) + '%';
+					}
+
+					// Inner span for mouseover tooltip.
+					$tdChart.append(
+						'<span class="chart chart-' + cssIndex.toString() + 
+						'" style="width: ' + cssWidth + ';">' + text + 
+						'<span class="tooltip">' + floatWidth.toFixed(0) + '% ' + labels[j] + '</span></span>'
+					);
+				}
+			}
+			
+		}
+
+	});
+});
+
+/**
+ * Parses a string in the format 'NUM (NUM%)' into 'NUM%'
+ */
+function parsePercentage (str) {
+	//console.log(str);
+	return str.match(/\d+ \((\d+\.\d+%)\)/)[1];
+}
+
+function parseFloatFromPercentage (str) {
+	return parseFloat(parsePercentage(str));
+}
